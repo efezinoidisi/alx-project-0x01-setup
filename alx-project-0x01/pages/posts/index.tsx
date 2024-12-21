e8c8034f0160cb8f86b15e340c1904d64ff1dbca
efezino <enduranceidisi13@gmail.com>
2024-12-21 14:18:26 +0100

    add basic folder structure

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/layout/Header';
+
+const PostsPage: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      PostsPage
+    </div>
+  );
+};
+
+export default PostsPage;
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/layout/Header';
+
+const UsersPage: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      users page
+    </div>
+  );
+};
+
+export default UsersPage;
